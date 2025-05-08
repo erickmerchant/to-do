40f44cd0a79f7528189ae89b20853a6a72719070
erickmerchant <erickmerchant@gmail.com>
2025-05-08 08:25:17 -0400

    Use lowercase

--- app.js
+++ app.js
@@ -8,16 +8,8 @@ import {h, $} from "handcraft/dom.js";
 import {watch, effect} from "handcraft/reactivity.js";
 import {when} from "handcraft/when.js";
 
-let {
-	input: INPUT,
-	label: LABEL,
-	h1: H1,
-	li: LI,
-	button: BUTTON,
-	ol: OL,
-	div: DIV,
-} = h.html;
-let {title: TITLE, path: PATH, svg: SVG} = h.svg;
+let {input, label, h1, li, button, ol, div} = h.html;
+let {title, path, svg} = h.svg;
 
 define("to-do-app").connected((host) => {
 	let state = watch(
@@ -36,10 +28,11 @@ define("to-do-app").connected((host) => {
 		localStorage.setItem("to-do-app", JSON.stringify(state));
 	});
 
-	let heading = H1.classes("title").text("To Do List");
+	let heading = h1.classes("title").text("To Do List");
 	let showDone = () =>
-		DIV.classes("show-done")(
-			INPUT.id("show-done")
+		div.classes("show-done")(
+			input
+				.id("show-done")
 				.type("checkbox")
 				.prop("checked", () => state.showDone)
 				.on("change", function () {
@@ -54,9 +47,10 @@ define("to-do-app").connected((host) => {
 
 					state.showDone = show;
 				}),
-			LABEL.for("show-done").text("Show done")
+			label.for("show-done").text("Show done")
 		);
-	let textInput = INPUT.classes("input-text")
+	let textInput = input
+		.classes("input-text")
 		.placeholder("What do you have to do?")
 		.on("keypress", function (e) {
 			if (e.key === "Enter") {
@@ -83,7 +77,8 @@ define("to-do-app").connected((host) => {
 	let itemsList = each(state.list)
 		.filter((value) => state.showDone || !value.isDone || value.isLeaving)
 		.map((value, index) => {
-			let toggleDoneCheckbox = INPUT.type("checkbox")
+			let toggleDoneCheckbox = input
+				.type("checkbox")
 				.id(() => `item-${index()}`)
 				.prop("checked", () => value.isDone)
 				.on("change", function () {
@@ -95,27 +90,29 @@ define("to-do-app").connected((host) => {
 
 					value.isDone = isDone;
 				});
-			let itemLabel = LABEL.for(() => `item-${index()}`).text(() => value.text);
-			let deleteButton = BUTTON.type("button")
+			let itemLabel = label.for(() => `item-${index()}`).text(() => value.text);
+			let deleteButton = button
+				.type("button")
 				.classes("delete")
 				.on("click", function () {
 					value.isLeaving = true;
 					value.isDeleted = true;
 				})(
-				SVG.viewBox("0 0 16 16")(
-					TITLE.text("Delete"),
-					PATH.d(
+				svg.viewBox("0 0 16 16")(
+					title.text("Delete"),
+					path.d(
 						"M4 1 L8 5 L12 1 L15 4 L11 8 L15 12 L12 15 L8 11 L4 15 L1 12 L5 8 L1 4 Z"
 					)
 				)
 			);
 
-			return LI.classes("item", {
-				done: () => value.isDone,
-				leaving: () => value.isLeaving,
-				entering: () => value.isEntering,
-				dragging: () => dragState.item === value(),
-			})
+			return li
+				.classes("item", {
+					done: () => value.isDone,
+					leaving: () => value.isLeaving,
+					entering: () => value.isEntering,
+					dragging: () => dragState.item === value(),
+				})
 				.prop("draggable", true)
 				.on("dragstart", function (e) {
 					dragState.item = value();
@@ -145,8 +142,8 @@ define("to-do-app").connected((host) => {
 					}
 				})(toggleDoneCheckbox, itemLabel, deleteButton);
 		})
-		.fallback(() => LI.classes("item").text("No items yet"));
-	let listOl = OL.classes("list")(itemsList);
+		.fallback(() => li.classes("item").text("No items yet"));
+	let listOl = ol.classes("list")(itemsList);
 
 	host(
 		heading,

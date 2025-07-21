5f0d9bb94cc75cd2c7a6389a6dcfdd971fd2c8ce
erickmerchant <erickmerchant@gmail.com>
2025-07-21 02:36:46 -0400

    Update

--- flint.ts
+++ flint.ts
@@ -1,10 +1,10 @@
 import css from "@flint/framework/plugins/css";
 import js from "@flint/framework/plugins/js";
-import create from "@flint/framework/create";
+import flint from "@flint/framework";
 import index from "./index.js";
 
-const app = create("public")
-	.cache(["/"])
+const app = flint("public")
+	.cache("/")
 	.route("/", index)
 	.route("/*.css", css)
 	.route("/app.js", js)

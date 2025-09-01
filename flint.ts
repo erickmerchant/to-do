9c3930e41160d2009f67126b033b0411f2190953
erickmerchant <erickmerchant@gmail.com>
2025-08-31 23:19:25 -0400

    Reorganize

--- flint.ts
+++ flint.ts
@@ -1,9 +1,9 @@
 import css from "@flint/framework/handlers/css";
 import js from "@flint/framework/handlers/js";
 import flint from "@flint/framework";
-import index from "./index.ts";
+import index from "./src/index.ts";
 
-const app = flint("public", "dist")
+const app = flint("src", "dist")
   .route("/", index)
   .file("/styles.css", css)
   .file("/app.js", js);

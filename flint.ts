d4732e50539343d34a7da3337c5c9196b36a97c8
erickmerchant <erickmerchant@gmail.com>
2025-10-04 01:25:08 -0400

    Upgrade flint

--- flint.ts
+++ flint.ts
@@ -4,9 +4,9 @@ import flint from "@flint/framework";
 import index from "./src/index.ts";
 
 const app = flint("src", "dist")
-  .route("/", { handler: index })
-  .file("/styles.css", { handler: css })
-  .file("/app.js", { handler: js });
+  .route("/", index)
+  .file("/styles.css", css)
+  .file("/app.js", js);
 
 export default app;
 
